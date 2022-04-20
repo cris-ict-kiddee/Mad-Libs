97f8a0f7b0cd04257877b4a8987b34d3de474350
Kento <ken.f@cris.ac.th>
2022-04-20 12:44:04 +0700

    added mad libs python file

---
+++ mad-libs.py
@@ -0,0 +1,37 @@
+#Team members: Kiddee, Kent
+#Life of Po
+
+""" Hi, my name is Po. I’m _(adjective 1)_, _(adjective 2)_, _(adjective 3)_ and __(adjective 4)__ boy. I live in the _(adjective 5)_ _(shape 1)_ _(colour 1)_ _(place 1)_ named _(named 1)_. My father is the _(job 1)_ who _(verb 1v1)_ a _(things 1)_ to _(name 2)__ _(place 2)_.Today is my __(sequence 1)__ _(Time expression 1)_ to go to _(place 3)__ since I _(verb 2v2)_ the old _(place 4)_. I _(verb 3v2)_ with Katty, my _(adjective 6)_ _(Relationship 1)_ who said to me “Don’t be worried about your smartphone and TV spying on you. Your vacuum cleaner has been gathering dirt on you for years”. Finally this is my _(sequence 2)_ dinner at _(restaurant name 1)_ with my _(relationship 2)_ after that I have to go to _(place 5)_. _(word expression 1)_, Po!!! """
+
+import random
+r = random.randrange(0,8)
+def Randomizer(Thing)
+	Thing[r]
+
+adjective1 = ['fat', 'morbidly obese', 'big', 'wide', 'skinny', 'lean', 'bony']
+adjective2 = ['fast', 'slow', 'swift', 'strong', 'weak', 'powerful', 'light', 'nice']
+adjective3 = ['slimy', 'loud', 'round', 'furry', 'evil', 'idiotic', 'bald', 'immense']
+adjective4 = ['nifty', 'homeless', 'serious', 'swanky', 'scrawny', 'ugly', 'hissing', 'wary', 'second-hand', 'jealous']
+adjective5 = ['big', 'small', 'smelly', 'trashy', 'sussy', 'bright', 'lovely', 'tiny']
+shape1 = ['circle', 'square', 'triangle', 'cube', 'hexagon', 'octagon', 'rhombus','decagon'] 
+Colour1 = ['red', 'blue', 'yellow', 'white', 'purple', 'rainbow', 'black', 'violet'] 
+Place1 = ['Toilet', 'House', 'Barn', 'supermarket', 'box', 'hole', 'restaurant', 'school'] 
+Name1 = input("Enter a name for your house:\n")
+#Job1 =
+#Verb1v1 = 
+#things1 = 
+name2 = input("Enter a name of a place:\n")
+place2 = ['school', 'restaurant', 'office', 'home', 'stadium', 'cave', 'college', 'hotel']
+sequence1 = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth']
+Time_expression1 = ['day', 'week', 'year', '2 weeks', '3 weeks', '3 days', '4 years', '6 years']
+place3 = ['school', 'restaurant', 'office', 'home', 'stadium', 'cave', 'college', 'hotel'] 
+verb2v2 = ['began', 'drank', 'went', 'ate', 'bought', 'built', 'drew', 'burnt']
+place4 = ['drug store', 'clinic', 'theater', 'bank', 'cinema', 'night Club', 'market', 'night market']
+verb3v2 = ['began', 'drank', 'went', 'ate', 'bought', 'built', 'drew', 'burnt']
+adjective6 = ['big', 'small', 'smelly', 'trashy', 'sussy', 'bright', 'lovely', 'tiny'] 
+Relationship1 = ['father', 'mather', 'sister', 'brother', 'grandmother', 'girlfriend', 'grandfather', 'aunt']
+sequence2 = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth']
+restaurant_name1 = ['big', 'small', 'smelly', 'trashy', 'sussy', 'bright', 'lovely', 'tiny']
+relationship2 = ['father', 'mather', 'sister', 'brother', 'grandmother', 'girlfriend', 'grandfather', 'aunt']
+place5 = ['school', 'resturange', 'office', 'home', 'stadium', 'cave', 'college', 'hotel']
+word_expression1 = ['Goodluck', 'Godspeed', 'Bon voyage', 'Goodbye', 'Adios', 'Adieu', 'Cheerio', 'Farewell']
